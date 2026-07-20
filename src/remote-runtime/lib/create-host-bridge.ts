6430d5dd5a4c9bd6ec53ab71480dbd25cf47b0c6
tryproxy <tryproxy@protonmail.com>
2026-07-20 21:03:27 +0400

    refactor(remote-runtime): unify host bridge snapshots

--- src/remote-runtime/lib/create-host-bridge.ts
+++ src/remote-runtime/lib/create-host-bridge.ts
@@ -7,6 +7,13 @@ import { createNoopTelemetry } from './create-noop-telemetry';
 
 type Listener = () => void;
 
+function subscribe(set: Set<Listener>, listener: Listener): () => void {
+  set.add(listener);
+  return () => {
+    set.delete(listener);
+  };
+}
+
 export function createHostBridge(
   initialTheme: ThemeMode,
   initialLocale: AppLocale
@@ -19,44 +26,44 @@ export function createHostBridge(
   let currentLocale = initialLocale;
   const themeListeners = new Set<Listener>();
   const localeListeners = new Set<Listener>();
+  const authListeners = new Set<Listener>();
+  const navigationListeners = new Set<Listener>();
+
+  const notifyNavigation = () => {
+    navigationListeners.forEach((listener) => listener());
+  };
+
+  if (typeof window !== 'undefined') {
+    window.addEventListener('popstate', notifyNavigation);
+  }
 
   const bridge: HostBridge = {
     theme: {
       getSnapshot: () => ({ mode: currentTheme }),
-      subscribe: (listener) => {
-        themeListeners.add(listener);
-
-        return () => {
-          themeListeners.delete(listener);
-        };
-      },
+      subscribe: (listener) => subscribe(themeListeners, listener),
     },
 
     i18n: {
-      getLocale: () => currentLocale,
-      subscribe: (listener) => {
-        localeListeners.add(listener);
-
-        return () => {
-          localeListeners.delete(listener);
-        };
-      },
+      getSnapshot: () => ({ locale: currentLocale }),
+      subscribe: (listener) => subscribe(localeListeners, listener),
     },
 
     auth: {
-      getSession: () => ({
+      getSnapshot: () => ({
         userId: 'dev-user',
         displayName: 'Dev User',
         roles: ['admin'],
       }),
+      subscribe: (listener) => subscribe(authListeners, listener),
     },
 
     navigation: {
-      getLocation: () => ({
+      getSnapshot: () => ({
         pathname: window.location.pathname,
         search: window.location.search,
         hash: window.location.hash,
       }),
+      subscribe: (listener) => subscribe(navigationListeners, listener),
 
       navigate: (path: string) => {
         window.history.pushState(null, '', path);
